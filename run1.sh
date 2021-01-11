84d01cd2dab9870bfffd2857c28924b3ce866871
TrungOre <krgaim10@gmail.com>
2021-01-11 16:58:44 +0700

    add all - still have bug

---
+++ run1.sh
@@ -0,0 +1,10 @@
+./genvrp ../Instances/CVRP/X-n327-k20.vrp result/X-n327-k20.sol -seed 9421 -t 785
+./genvrp ../Instances/CVRP/X-n331-k15.vrp result/X-n331-k15.sol -seed 9421 -t 795
+./genvrp ../Instances/CVRP/X-n336-k84.vrp result/X-n336-k84.sol -seed 9421 -t 807
+./genvrp ../Instances/CVRP/X-n344-k43.vrp result/X-n344-k43.sol -seed 9421 -t 826
+./genvrp ../Instances/CVRP/X-n351-k40.vrp result/X-n351-k40.sol -seed 9421 -t 843
+
+
+
+
+
---
+++ run2.sh
@@ -0,0 +1,10 @@
+./genvrp ../Instances/CVRP/X-n359-k29.vrp result/X-n359-k29.sol -seed 9421 -t 862
+./genvrp ../Instances/CVRP/X-n367-k17.vrp result/X-n367-k17.sol -seed 9421 -t 881
+./genvrp ../Instances/CVRP/X-n376-k94.vrp result/X-n376-k94.sol -seed 9421 -t 903
+./genvrp ../Instances/CVRP/X-n384-k52.vrp result/X-n384-k52.sol -seed 9421 -t 922
+./genvrp ../Instances/CVRP/X-n393-k38.vrp result/X-n393-k38.sol -seed 9421 -t 943
+
+
+
+
+
